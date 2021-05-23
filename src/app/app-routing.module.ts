996813347341b37ad07a280d50c9ec4ba998b639
Anand Nayak <anandnayak95@gmail.com>
2021-05-23 22:29:55 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   component:ListStudnetComponent,
+  //   path:'listStudent'
+  // },
+  // {
+  //   component:LoginComponent,
+  //   path:'login'
+  // },
+  // {
+  //   component:RegisterStudentComponent,
+  //   path:'registerStudent'
+  // },
+  // {
+  //   component:TemplateDrivenComponent,
+  //   path:'tempDriven'
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
